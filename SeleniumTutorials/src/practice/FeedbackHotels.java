d0e1753766c01a3da170ccf0b698c0ebf9f10a76
kp <kpr953@gmail.com>
2016-11-22 17:38:59 -0500

    commit-2

--- SeleniumTutorials/src/practice/FeedbackHotels.java
+++ SeleniumTutorials/src/practice/FeedbackHotels.java
@@ -12,25 +12,25 @@ public class FeedbackHotels extends Web{
 	@Test
 	public void fillDetails() throws InterruptedException{
 		
-		getDriver().findElement(By.id("hdr-feedback")).click();
+		webDriverFluentWait(By.id("hdr-feedback")).click();
 		switchToWindow(1);
 		Thread.sleep(5000);
-		getDriver().findElement(By.id("comment-textarea")).sendKeys("This website time complxity is too bad");
-		getDriver().findElement(By.id("overall-1")).click();
-		getDriver().findElement(By.id("content-2")).click();
-		getDriver().findElement(By.id("design-3")).click();
-		getDriver().findElement(By.id("usability-4")).click();
-		Select dropdown = new Select(getDriver().findElement(By.id("4293415")));
+		webDriverFluentWait(By.id("comment-textarea")).sendKeys("This website time complxity is too bad");
+		webDriverFluentWait(By.id("overall-1")).click();
+		webDriverFluentWait(By.id("content-2")).click();
+		webDriverFluentWait(By.id("design-3")).click();
+		webDriverFluentWait(By.id("usability-4")).click();
+		Select dropdown = new Select(webDriverFluentWait(By.id("4293415")));
 		List<WebElement> option =  dropdown.getOptions();
 		for(WebElement dropdownList:option){
 			if(dropdownList.getText().equals("Somewhat Likely")){
 				dropdownList.click();
 			}
 		}
-		getDriver().findElement(By.id("answer_4293414-2")).click();
-		getDriver().findElement(By.id("answer_4293413-1")).click();
-		getDriver().findElement(By.id("email_address")).sendKeys("kpr@gmail.com");
-		getDriver().findElement(By.xpath("html/body/form/footer/div/div/div[1]/input")).click();
+		webDriverFluentWait(By.id("answer_4293414-2")).click();
+		webDriverFluentWait(By.id("answer_4293413-1")).click();
+		webDriverFluentWait(By.id("email_address")).sendKeys("kpr@gmail.com");
+		webDriverFluentWait(By.xpath("html/body/form/footer/div/div/div[1]/input")).click();
 		switchToRootCloseCurrent();
 			
 		
